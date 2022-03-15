c4c9a970f4f8ba0672bd4607dbc4ffbcc4fd53ef
ChuckMac <chuck@chuckmacdev.com>
2022-03-15 10:34:26 -0400

    Adding test for WDW auth

---
+++ adrfinder/tests/test_auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from adrfinder import fetch_site_status
+
+
+def test_wdw_auth(app, client):
+    datastore = ADRFinderStore_test()
+    update_handler = fetch_site_status.perform_site_check(datastore=datastore)
+    request_header = update_handler.get_auth_header()
+    assert len(request_header["Authorization"]) > 7
+
+
+class ADRFinderStore_test:
+
+    def __init__(self):
+        self.data = {
+            'cache': {
+                'auth': {
+                    'auth_token_expiry': 0,
+                }
+            }
+        }
+
+    def update_auth(self, auth_token, auth_token_expiry):
+        return True
